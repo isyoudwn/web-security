4cd9c9fd0957bd0452112167963f8f5fcb758e23
임채연 <dlacodus0407@gmail.com>
2023-06-21 19:25:32 +0900

    게시물 발행 구현

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 const express = require('express');
 const app = express();
-app.use(express.urlencoded({extended: true})) 
-
+app.use(express.urlencoded({extended: true}));
+let db;
+app.set('view engine', 'ejs');
 
 const MongoClient = require('mongodb').MongoClient;
 
-let db;
+
 MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.wmnmkps.mongodb.net/?retryWrites=true&w=majority', (err, client) => {
     if (err) return console.log(err);
 
     db = client.db('todoapp');
-
-    db.collection('post');
-
-    db.collection('post').insertOne({ 이름 : 'yuze', 나이 : 20 }, function(err, data) {
-        console.log('저장완료');
-    });
 
     app.listen(8080,() => {
         console.log("listening on 8080");
@@ -24,13 +19,32 @@ MongoClient.connect('mongodb+srv://admin:admin1234@cluster0.wmnmkps.mongodb.net/
 
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.render("index.ejs");
 });
 
 app.get('/write', (req, res) => {
-    res.sendFile(__dirname + '/write.html');
+    res.render('write.ejs');
 });
 
 app.post('/add', (req, res) => {
-    res.send('전송완료');
+    db.collection('counter').findOne({name : '게시물개수'}, (err, result) => {
+        let totalPost = result.totalPost;
+
+        db.collection('post').insertOne({ _id: totalPost + 1, 제목 : req.body.title, 날짜 : req.body.date}, (err, data) => {
+            /** update류의 함수를 사용하기 위해서는 operator를 사용해야 함.
+             * set은 '데이터를 아예 바꿔주세요'
+             * inc는 '해당 숫자를 더해주세요'*/ 
+            db.collection('counter').updateOne({ name : '게시물개수' }, { $inc : { totalPost : 1 } }, (err, result) => {
+                if(err) {console.log(err)};
+                res.send("전송완료");
+            });
+        });
+    })
+});
+
+app.get('/list', (req, res) => {
+    db.collection('post').find().toArray((err, result) => {
+        console.log(result);
+        res.render('list.ejs', { posts : result });
+    });
 });
